a3db841cabdece6a41642446be45a151e6b18c66
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-08-20 09:32:05 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Lahiru Amarasooriya on 2023-08-20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
